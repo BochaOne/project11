7b0d3c5ccc17aa73ca29a91cf728ebfb4b59ae0e
Alexander <boch-2001@mail.ru>
2021-06-28 16:37:11 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project11';
+  
+  noteList: MyNote[] = [];
+
+  addNote(title: HTMLInputElement, text: HTMLTextAreaElement) {
+    let now = new Date();
+
+    let note: MyNote = {
+      noteTitle: title.value,
+      noteText: text.value,
+      noteDate: now.toLocaleString(),
+      noteId: this.noteList.length > 0
+        ? this.noteList[this.noteList.length - 1].noteId + 1
+        : 0
+    };
+    // title.value = "";
+    // text.value = "";
+    if (note.noteTitle != "") {
+      console.log(note);
+      this.noteList.push(note);
+    }
+    else alert("Заполните название!");
+  }
+
+  deleteNote(id: number) { 
+    let index = this.noteList.findIndex((note) => note.noteId === id)
+    if (index !== -1) {
+      this.noteList.splice(index, 1);
+    }
+  }
+}
+
+interface MyNote {
+  noteTitle: string;
+  noteText: string;
+  noteDate: string;
+  noteId: number;
 }
